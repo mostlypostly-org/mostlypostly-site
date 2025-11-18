d8b408f216ab4dacb4da112c170da52a6b979d31
Troy Hardister <troyhardister@Troys-MacBook-Pro.local>
2025-11-18 14:44:27 -0500

    Add staging site with local Tailwind build + env login

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        mpbg: "#0B1223",
+        mpcard: "#111827"
+      }
+    }
+  },
+  plugins: []
+};
